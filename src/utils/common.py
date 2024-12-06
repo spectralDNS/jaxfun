5dd7fd0378abfabfbd78c47946c6d039844b14ed
August Femtehjell <august.femtehjell@gmail.com>
2024-12-06 17:54:27 +0100

    Format files

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,13 +1,18 @@
 from typing import Callable
-import jax 
-from jax import Array 
 
-def diff(fun : Callable[[float, Array], float], k : int = 1):
+import jax
+from jax import Array
+
+
+def diff(
+    fun: Callable[[float, Array], float], k: int = 1
+) -> Callable[[Array, Array], Array]:
     for _ in range(k):
         fun = jax.grad(fun)
     return jax.jit(jax.vmap(fun, in_axes=(0, None)))
 
-def diffx(fun : Callable[[float], float], k : int = 1):
+
+def diffx(fun: Callable[[float], float], k: int = 1) -> Callable[[Array], Array]:
     for _ in range(k):
         fun = jax.grad(fun)
     return jax.jit(jax.vmap(fun))

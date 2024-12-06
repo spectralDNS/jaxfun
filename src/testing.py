b243eb946967d49612126a703c6f397e18d3ca32
August Femtehjell <august.femtehjell@gmail.com>
2024-12-06 18:32:50 +0100

    Refactor timings

--- src/testing.py
+++ src/testing.py
@@ -10,60 +10,107 @@ import Legendre
 jnp.set_printoptions(4)
 jax.config.update("jax_enable_x64", True)
 
-N = 10
+N = 1000
 M = 1000
-C = 10
+C = 1000
 x = jnp.linspace(-1, 1, N)
 xn = np.array(x)
 c = jnp.ones(C)
 cn = np.array(c)
-print("Legendre - Vandermonde")
-Legendre.legvander(x, N)
-print(
-    f'{"legvander":20s} {timeit.timeit(f"Legendre.legvander(x, {N})", number=M, setup="from __main__ import Legendre, x"):.4e}'
-)
-print(
-    f'{"np.legvander":20s} {timeit.timeit(f"np.polynomial.legendre.legvander(xn, {N-1})", number=M, setup="from __main__ import np, xn"):.4e}'
-)
-assert (
-    jnp.linalg.norm(
-        jnp.array(np.polynomial.legendre.legvander(xn, N - 1))
-        - Legendre.legvander(x, N)
+
+
+def run_legvander() -> None:
+    print("Legendre - Vandermonde")
+    Legendre.legvander(x, N)
+
+    time_jax = timeit.timeit(
+        f"Legendre.legvander(x, {N})",
+        number=M,
+        setup="from __main__ import Legendre, x",
     )
-    < 1e-8
-)
-
-print("Legendre - evaluate")
-Legendre.legval(x, c)
-print(
-    f'{"legval":20s} {timeit.timeit("Legendre.legval(x, c)", number=M, setup="from __main__ import Legendre, x, c"):.4e}'
-)
-print(
-    f'{"np.legval":20s} {timeit.timeit("np.polynomial.legendre.legval(xn, cn)", number=M, setup="from __main__ import np, xn, cn"):.4e}'
-)
-
-print("Chebyshev - Vandermonde")
-Chebyshev.chebvander(x, N)
-print(
-    f'{"chebvander":20s} {timeit.timeit(f"Chebyshev.chebvander(x, {N})", number=M, setup="from __main__ import Chebyshev, x"):.4e}'
-)
-Chebyshev.chebvander(x, N)
-print(
-    f'{"np.chebvander":20s} {timeit.timeit(f"np.polynomial.chebyshev.chebvander(xn, {N-1})", number=M, setup="from __main__ import np, xn"):.4e}'
-)
-assert (
-    jnp.linalg.norm(
-        jnp.array(np.polynomial.chebyshev.chebvander(x, N - 1))
-        - Chebyshev.chebvander(x, N)
+    print(f'{"legvander":20s} {time_jax:.4e}')
+    time_np = timeit.timeit(
+        f"np.polynomial.legendre.legvander(xn, {N - 1})",
+        number=M,
+        setup="from __main__ import np, xn",
     )
-    < 1e-8
-)
+    print(f'{"np.legvander":20s} {time_np:.4e}')
 
-print("Chebyshev - evaluate")
-Chebyshev.chebval(x, c)
-print(
-    f'{"chebval":20s} {timeit.timeit("Chebyshev.chebval(x, c)", number=M, setup="from __main__ import Chebyshev, x, c"):.4e}'
-)
-print(
-    f'{"np.chebval":20s} {timeit.timeit("np.polynomial.chebyshev.chebval(xn, cn)", number=M, setup="from __main__ import np, xn, cn"):.4e}'
-)
+    assert (
+        jnp.linalg.norm(
+            jnp.array(np.polynomial.legendre.legvander(xn, N - 1))
+            - Legendre.legvander(x, N)
+        )
+        < 1e-8
+    )
+
+
+def run_legval() -> None:
+    print("Legendre - evaluate")
+
+    Legendre.legval(x, c)
+    time_jax = timeit.timeit(
+        "Legendre.legval(x, c)",
+        number=M,
+        setup="from __main__ import Legendre, x, c",
+    )
+    print(f'{"legval":20s} {time_jax:.4e}')
+
+    time_np = timeit.timeit(
+        "np.polynomial.legendre.legval(xn, cn)",
+        number=M,
+        setup="from __main__ import np, xn, cn",
+    )
+    print(f'{"np.legval":20s} {time_np:.4e}')
+
+
+def run_chebvander() -> None:
+    print("Chebyshev - Vandermonde")
+    Chebyshev.chebvander(x, N)
+    time_jax = timeit.timeit(
+        f"Chebyshev.chebvander(x, {N})",
+        number=M,
+        setup="from __main__ import Chebyshev, x",
+    )
+    print(f'{"chebvander":20s} {time_jax:.4e}')
+
+    Chebyshev.chebvander(x, N)
+    time_np = timeit.timeit(
+        f"np.polynomial.chebyshev.chebvander(xn, {N - 1})",
+        number=M,
+        setup="from __main__ import np, xn",
+    )
+    print(f'{"np.chebvander":20s} {time_np:.4e}')
+
+    assert (
+        jnp.linalg.norm(
+            jnp.array(np.polynomial.chebyshev.chebvander(x, N - 1))
+            - Chebyshev.chebvander(x, N)
+        )
+        < 1e-8
+    )
+
+
+def run_chebval() -> None:
+    print("Chebyshev - evaluate")
+    Chebyshev.chebval(x, c)
+    time_jax = timeit.timeit(
+        "Chebyshev.chebval(x, c)",
+        number=M,
+        setup="from __main__ import Chebyshev, x, c",
+    )
+    print(f'{"chebval":20s} {time_jax:.4e}')
+
+    time_np = timeit.timeit(
+        "np.polynomial.chebyshev.chebval(xn, cn)",
+        number=M,
+        setup="from __main__ import np, xn, cn",
+    )
+    print(f'{"np.chebval":20s} {time_np:.4e}')
+
+
+if __name__ == "__main__":
+    run_legvander()
+    run_legval()
+    run_chebvander()
+    run_chebval()

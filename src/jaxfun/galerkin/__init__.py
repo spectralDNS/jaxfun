9bc5adcfc8bb8b10defd72b5b6dd22853fcebeeb
August Femtehjell <august.femtehjell@gmail.com>
2025-08-27 16:34:47 +0200

    Fix broken import (oops)

--- src/jaxfun/galerkin/__init__.py
+++ src/jaxfun/galerkin/__init__.py
@@ -9,6 +9,7 @@ from .arguments import (
     TestFunction as TestFunction,
     TrialFunction as TrialFunction,
 )
+from .composite import Composite as Composite
 from .functionspace import FunctionSpace as FunctionSpace
 from .inner import inner as inner
 from .tensorproductspace import (

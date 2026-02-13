63b79b73ceeb4328ce06e28c4ab4ac6872e19dfe
August Femtehjell <august.femtehjell@gmail.com>
2026-02-13 15:11:07 +0100

    extremely weird fix for broken tests
    Adding @nnx.jit to RK4.step removes the flax tracing errors, but adding
    @nnx.jit to ETDRK4.step introduces new tracing errors? This is very
    weird behaviour, and indicates that the approach with self.module.kernel
    in the nonlinear part is fundamentally broken.

--- src/jaxfun/integrators/rk4.py
+++ src/jaxfun/integrators/rk4.py
@@ -1,3 +1,5 @@
+from flax import nnx
+
 from jaxfun.typing import Array
 
 from .base import BaseIntegrator
@@ -6,6 +8,7 @@ from .base import BaseIntegrator
 class RK4(BaseIntegrator):
     """Regular 4th-order Runge-Kutta integrator."""
 
+    @nnx.jit
     def step(self, u_hat: Array, dt: float) -> Array:
         k1 = self.total_rhs(u_hat)
         k2 = self.total_rhs(u_hat + 0.5 * dt * k1)
